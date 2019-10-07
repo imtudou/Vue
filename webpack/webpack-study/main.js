dce870d696e917f83b520d35d709298b42eb2107
imtudou <imtudous@163.com>
2019-10-07 20:40:17 +0800

    vue study

---
+++ webpack/webpack-study/main.js
@@ -0,0 +1,14 @@
+//项目入口文件
+
+// 1.导入 jquery
+
+import $ from 'jquery'
+
+
+$(function(){
+    $('li:odd').css('backgroundColor','red')
+    $('li:even').css('backgroundColor',function(){
+        return '#'+'D97643'
+    })
+})
+
