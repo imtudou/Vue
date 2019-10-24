aa6f375992aa5b8a0b8878097c904530f43ee460
imtudou <imtudous@163.com>
2019-10-24 15:14:15 +0800

    vue-router 路由动态传参 query和params的区别

--- 03.vue-cms/src/components/photos/PhotoInfo.vue
+++ 03.vue-cms/src/components/photos/PhotoInfo.vue
@@ -2,18 +2,22 @@
   <div class="photoinfo_container">
     <h3 class="photoinfo_title">标题</h3>
     <p class="photoinfo_subtitle">
-      <span>发表时间:</span>
-      <span>点击次数:</span>
+      <span>发表时间:{{this.$route}}</span>
+      <span>点击次数:0次</span>
     </p>
     <hr />
     <!--内容区域-->
-    <div class="photoinfo_content" v-html="PhotoInfoList.contents"></div>
+    <div class="photoinfo_content"></div>
     <!--评论子组件区域-->
     <comment-box></comment-box>
   </div>
 </template>
 
 <script>
+
+
+
+
 import { Toast } from "mint-ui";
 
 //导入子组件
@@ -26,7 +30,7 @@ export default {
     };
   },
   methods: {},
-  components: {
+  components: {//注册评论子组件
     "comment-box": comment
   }
 };
@@ -34,15 +38,6 @@ export default {
 
 
 <style lang="scss" scoped>
-.photoinfo_container {
-  .photoinfo_title {
-  }
-  .photoinfo_subtitle {
-  }
-  .photoinfo_content {
-  }
-}
-
 
 .photoinfo_container {
   padding: 0 5px;
@@ -51,13 +46,18 @@ export default {
     font-size: 16px;
     text-align: center;
     margin: 15px 0;
-    color: red;
+    color: #26A2FF;
   }
   .photoinfo_subtitle {
     font-size: 13px;
     color: #26a2ff;
     display: flex;
     justify-content: space-between;
+  }
+  .photoinfo_content{
+    font-size: 13px;
+    line-height: 30px;
+
   }
 
  
